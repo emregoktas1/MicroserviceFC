98d1adab59f6c40449612e772c84e9c743aa3a13
Emre <egoktas@menarini.com.tr>
2024-08-13 15:09:21 +0300

    Shared Library Created Dtos created -> CoContent ->ResponseDto

---
+++ Shared/FreeCourse.Shared/Dtos/ResponseDto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace FreeCourse.Shared.Dtos
+{
+    public class ResponseDto<T>
+    {
+        public T Data { get; private set; }
+
+        [JsonIgnore]
+        public int Status { get; private set; }
+
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+
+        public List<string> Errors { get; set; }
+
+        public static ResponseDto<T> Success(T data, int statusCode)
+        {
+            return new ResponseDto<T> { Status = statusCode, Data = data, IsSuccessful = true };
+        }
+
+        public static ResponseDto<T> Success(int statusCode)
+        {
+            return new ResponseDto<T> { Data = default(T), Status = statusCode, IsSuccessful = true };
+        }
+
+        public static ResponseDto<T> Fail(List<string> errors, int statusCode)
+        {
+            return new ResponseDto<T>
+            {
+                Errors = errors,
+                Status = statusCode,
+                IsSuccessful = false
+            };
+        }
+
+        public static ResponseDto<T> Fail(string error, int statusCode)
+        {
+            return new ResponseDto<T>
+            {
+                Errors = new List<string>() { error },
+                Status = statusCode,
+                IsSuccessful = false
+            };
+        }
+
+    }
+}
